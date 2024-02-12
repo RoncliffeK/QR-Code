ce3f7a931786059d834bee156d2f98fa140e7db3
RoncliffeK <124488338+RoncliffeK@users.noreply.github.com>
2024-02-12 12:29:45 +0300

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,11 @@
+import qrcode
+
+qr = qrcode.QRCode(version=1, box_size=10, border=5)
+
+qr.add_data("https://www.nrt-kenya.org")
+
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save("qr.png", "PNG")
